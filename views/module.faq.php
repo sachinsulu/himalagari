e50d16f6f91e65bdf654b40a860b7258f6125a0f
Sachin Sulu <sachin.sulu324@gmail.com>
2026-04-09 12:18:29 +0545

    feat: implement FAQ module with CRUD functionality and admin panel management

---
+++ views/module.faq.php
@@ -0,0 +1,51 @@
+<?php
+$resfaq = '';
+
+$faqRec = Faq::find_all();
+
+if ($faqRec) {
+    $resfaq .= '
+    <section class="faq-wrapper components">
+        <h4 class="text-center mb-2">
+            Frequently Asked Questions
+        </h4>
+
+        <h2 class="text-center green-title mb-5">
+            Everything you need to <span class="orange-text">know.</span>
+        </h2>
+
+        <div class="accordion container faq_content" id="accordionExample">';
+
+    foreach ($faqRec as $key => $faqRow) {
+        $show = ($key == 0) ? 'show' : '';
+        $collapsed = ($key == 0) ? '' : 'collapsed';
+        $expanded = ($key == 0) ? 'true' : 'false';
+
+        $resfaq .= '
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button ' . $collapsed . '" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#faq' . $faqRow->id . '" aria-expanded="' . $expanded . '" aria-controls="faq' . $faqRow->id . '">
+                        ' . $faqRow->title . '
+                    </button>
+                </h2>
+
+                <div id="faq' . $faqRow->id . '" class="accordion-collapse collapse ' . $show . '" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        ' . $faqRow->content . '
+                    </div>
+                </div>
+            </div>';
+    }
+
+    $resfaq .= '
+        </div>
+
+        <button class="explore_btn inquiry-btn mx-auto mt-4">
+            View More
+        </button>
+    </section>';
+}
+
+$jVars['module:faq-destination'] = $resfaq;
+?>
